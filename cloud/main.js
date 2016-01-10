a40c80ce04f2f7f81fb331494826037cbb0f1921
Afik Cohen <aphex@aphex.cx>
2016-01-10 00:38:40 -0800

    Reformat and cleanup code.

--- cloud/main.js
+++ cloud/main.js
@@ -1,36 +1,36 @@
 require("cloud/app.js");
 
-require("cloud/login.js")
+require("cloud/login.js");
 
-Parse.Cloud.define("claimPickupRequest", function(req, res) {
-	if (req.params.pickupRequestId) {
+Parse.Cloud.define("claimPickupRequest", function (req, res) {
+    if (req.params.pickupRequestId) {
         var volunteer = req.user;
         var donor = new Parse.User();
 
         var PickupRequest = Parse.Object.extend("PickupRequest");
         var query = new Parse.Query(PickupRequest);
-        query.equalTo('objectId', req.params.pickupRequestId +"");
-        query.first().then(function(pickupRequest) {
+        query.equalTo('objectId', req.params.pickupRequestId + "");
+        query.first().then(function (pickupRequest) {
             if (pickupRequest) {
-        			pickupRequest.set("pendingVolunteer", volunteer);
+                pickupRequest.set("pendingVolunteer", volunteer);
 
-        			donor.id = pickupRequest.get("donor").id;
-                    //Let the donor read the volunteer's user object
-                    var volACL = new Parse.ACL(volunteer);
-                    volACL.setReadAccess(donor.id,true);
-                    volunteer.setACL(volACL);
-                    Parse.Object.saveAll([pickupRequest, volunteer]).then(function() {
-                        res.success("Request claimed, ACL of volunteer "+ volunteer.id +" updated to allow read access for user "+ donor.id);
-                    }, function(err) {
-                        res.error(err);
-                    });
+                donor.id = pickupRequest.get("donor").id;
+                //Let the donor read the volunteer's user object
+                var volACL = new Parse.ACL(volunteer);
+                volACL.setReadAccess(donor.id, true);
+                volunteer.setACL(volACL);
+                Parse.Object.saveAll([pickupRequest, volunteer]).then(function () {
+                    res.success("Request claimed, ACL of volunteer " + volunteer.id + " updated to allow read access for user " + donor.id);
+                }, function (err) {
+                    res.error(err);
+                });
             } else {
-                res.error("No result for query for pickupRequest with id "+ req.params.pickupRequestId);
+                res.error("No result for query for pickupRequest with id " + req.params.pickupRequestId);
             }
         }, function (err) {
             res.error(err);
         });
     } else {
-		res.error('Invalid parameters.');
-	}
+        res.error('Invalid parameters.');
+    }
 });
