fc26428f524df96a222d85725de5f52e54e9d51f
Afik Cohen <aphex@aphex.cx>
2016-01-13 01:32:56 -0800

    Send push notification to donor via cloud code when their pickup request is claimed.

--- cloud/main.js
+++ cloud/main.js
@@ -6,31 +6,59 @@ Parse.Cloud.define("claimPickupRequest", function (req, res) {
     if (req.params.pickupRequestId) {
         var volunteer = req.user;
         var donor = new Parse.User();
-
         var PickupRequest = Parse.Object.extend("PickupRequest");
         var query = new Parse.Query(PickupRequest);
         query.equalTo('objectId', req.params.pickupRequestId + "");
         query.first().then(function (pickupRequest) {
-            if (pickupRequest) {
-                pickupRequest.set("pendingVolunteer", volunteer);
+            pickupRequest.set("pendingVolunteer", volunteer);
 
-                donor.id = pickupRequest.get("donor").id;
-                //Let the donor read the volunteer's user object
-                var volACL = new Parse.ACL(volunteer);
-                volACL.setReadAccess(donor.id, true);
-                volunteer.setACL(volACL);
-                Parse.Object.saveAll([pickupRequest, volunteer]).then(function () {
-                    res.success("Request claimed, ACL of volunteer " + volunteer.id + " updated to allow read access for user " + donor.id);
+            donor.id = pickupRequest.get("donor").id;
+            //Let the donor read the volunteer's user object
+            var volACL = new Parse.ACL(volunteer);
+            volACL.setReadAccess(donor.id, true);
+            volunteer.setACL(volACL);
+            Parse.Object.saveAll([pickupRequest, volunteer]).then(function () {
+                volunteerName = volunteer.get("name") || "A volunteer";
+                generatePushToUser(donor,
+                    "Is your donation ready?",
+                    volunteerName + " wants to pick up your donation. Confirm?",
+                    "confirmPendingVolunteer").then(function () {
+                    res.success("Request claimed, ACL of volunteer " + volunteer.id + " updated to allow read access for user " + donor.id + ". confirmPendingVolunteer Push Notification sent to " + donor.id);
                 }, function (err) {
                     res.error(err);
                 });
-            } else {
-                res.error("No result for query for pickupRequest with id " + req.params.pickupRequestId);
-            }
-        }, function (err) {
-            res.error(err);
+            });
         });
     } else {
         res.error('Invalid parameters.');
     }
 });
+
+
+function generatePushToUser(user, title, alert, type) {
+    //Set push query
+    var pushQuery = new Parse.Query(Parse.Installation);
+    pushQuery.equalTo("user", user);
+
+    var promise = new Parse.Promise();
+
+    //Send Push message
+    Parse.Push.send({
+        where: pushQuery,
+        data: {
+            title: title,
+            alert: alert,
+            type: type
+        }
+    }, {
+        success: function () {
+            promise.resolve();
+        },
+        error: function (error) {
+            console.log(error);
+            promise.reject(error.message);
+        }
+    });
+    return promise;
+
+}
