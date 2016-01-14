ba66223f307ebb9fe283a7b4e79ed39c66170377
Afik Cohen <aphex@aphex.cx>
2016-01-13 19:34:03 -0800

    Migrate confirm volunteer functionality to cloud code.

--- cloud/main.js
+++ cloud/main.js
@@ -10,15 +10,19 @@ Parse.Cloud.define("claimPickupRequest", function (req, res) {
         var query = new Parse.Query(PickupRequest);
         query.equalTo('objectId', req.params.pickupRequestId + "");
         query.first().then(function (pickupRequest) {
+            // Update the pickupRequest object with the pending volunteer.
             pickupRequest.set("pendingVolunteer", volunteer);
 
             donor.id = pickupRequest.get("donor").id;
-            // Let the donor read the volunteer's user object
+
+            // Let the donor read the volunteer's user object, so that she can read the volunteer's name.
             var volACL = new Parse.ACL(volunteer);
             volACL.setReadAccess(donor.id, true);
             volunteer.setACL(volACL);
             Parse.Object.saveAll([pickupRequest, volunteer]).then(function () {
                 var volunteerName = volunteer.get("name");
+
+                // Send a push to the donor notifying them their pickup request has been claimed.
                 generatePushToUser(donor,
                     {
                         "loc-key": "notif_pickup_request_claimed_title",
@@ -30,9 +34,58 @@ Parse.Cloud.define("claimPickupRequest", function (req, res) {
                         "action-loc-key": "rsp"
                     },
                     "claimPickupRequest").then(function () {
-                    res.success("Request claimed, ACL of volunteer " + volunteer.id + " updated to allow read access for user " + donor.id + ". confirmPendingVolunteer Push Notification sent to " + donor.id);
+                    res.success("Request claimed, ACL of volunteer " + volunteer.id + " updated to allow read access for user " + donor.id + ". claimPickupRequest Push Notification sent to " + donor.id);
                 }, function (err) {
                     res.error(err);
+                });
+            });
+        });
+    } else {
+        res.error('Invalid parameters.');
+    }
+});
+
+
+Parse.Cloud.define("confirmVolunteer", function (req, res) {
+    if (req.params.pickupRequestId && req.params.volunteerId) {
+        var donor = req.user;
+        var PickupRequest = Parse.Object.extend("PickupRequest");
+        var query = new Parse.Query(PickupRequest);
+        query.equalTo('objectId', req.params.pickupRequestId + "");
+        query.equalTo('donor', donor.id);
+        query.first().then(function (pickupRequest) {
+            // Get the volunteer user object.
+            var volunteerQuery = new Parse.Query(User);
+            volunteerQuery.equalTo('objectId', req.params.volunteerId);
+            query.first().then(function (volunteer) {
+                // Update the pickupRequest object with the confirmed volunteer.
+                pickupRequest.set("confirmedVolunteer", volunteer);
+                donor.id = pickupRequest.get("donor").id;
+
+                // Let the volunteer read the donor's user object, because she will need it to call/msg/etc the donor.
+                var donorACL = new Parse.ACL(donor);
+                donorACL.setReadAccess(volunteer.id, true);
+                donor.setACL(volACL);
+
+                Parse.Object.saveAll([pickupRequest, volunteer]).then(function () {
+                    var donorName = donor.get("name");
+
+                    // Send a push to the volunteer notifying them the donor confirmed them as a volunteer.
+                    generatePushToUser(volunteer,
+                        {
+                            "loc-key": "notif_volunteer_confirmed_title",
+                            "loc-args": []
+                        },
+                        {
+                            "loc-key": "notif_volunteer_confirmed_msg",
+                            "loc-args": donorName ? [donorName] : [],
+                            "action-loc-key": "rsp"
+                        },
+                        "confirmVolunteer").then(function () {
+                        res.success("Volunteer confirmed, ACL of donor " + donor.id + " updated to allow read access for user " + volunteer.id + ". confirmVolunteer Push Notification sent to " + volunteer.id);
+                    }, function (err) {
+                        res.error(err);
+                    });
                 });
             });
         });
