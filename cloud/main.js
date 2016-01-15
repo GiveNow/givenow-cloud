923df15d8f8d3c597805352c174f0cdb308ad260
Afik Cohen <aphex@aphex.cx>
2016-01-15 15:56:39 -0800

    Add first iteration of pickupDonation cloud function. success/error is not being called

--- cloud/main.js
+++ cloud/main.js
@@ -98,6 +98,60 @@ Parse.Cloud.define("confirmVolunteer", function (req, res) {
     }
 });
 
+Parse.Cloud.define("pickupDonation", function (req, res) {
+    if (req.params.pickupRequestId) {
+        var volunteer = req.user;
+        var donor = new Parse.User();
+        var PickupRequest = Parse.Object.extend("PickupRequest");
+        var query = new Parse.Query(PickupRequest);
+        query.equalTo('objectId', req.params.pickupRequestId + "");
+        query.first().then(function (pickupRequest) {
+            donor.id = pickupRequest.get("donor").id;
+
+            // Create new Donation object
+            var Donation = Parse.Object.extend("Donation");
+            var newDonation = new Donation();
+            newDonation.set("donor", donor.id);
+            newDonation.set("donationCategories", pickupRequest.get("donationCategories"));
+            // Readable by the volunteer and donor
+            var donationACL = new Parse.ACL();
+            donationACL.setReadAccess(volunteer.id, true);
+            donationACL.setReadAccess(donor.id, true);
+            newDonation.setACL(donationACL);
+            console.log("newDonationSet");
+
+            newDonation.save().then(function (donation) {
+                console.log("saving pickup request");
+
+                pickupRequest.set("donation", donation.id);
+                pickupRequest.save().then(function () {
+                    var volunteerName = volunteer.get("name");
+                    console.log("newPickupREquestSaved");
+
+                    // Send a push to the donor notifying them their pickup request has been picked up
+                    generatePushToUser(donor,
+                        {
+                            "loc-key": "notif_pickup_complete_title",
+                            "loc-args": []
+                        },
+                        {
+                            "loc-key": "notif_pickup_complete_msg",
+                            "loc-args": volunteerName ? [volunteerName] : [],
+                            "action-loc-key": "rsp"
+                        },
+                        "pickupDonation").then(function () {
+                        res.success("Request picked up. New donation object " + donation.id + "created. pickupDonation Push Notification sent to " + donor.id);
+                    }, function (err) {
+                        res.error(err);
+                    });
+                });
+            });
+        });
+    } else {
+        res.error('Invalid parameters.');
+    }
+});
+
 
 function generatePushToUser(user, title, alert, type) {
     //Set push query
