5c0a6e3419cae3b0f83f3db7912eebcc56f6ecc1
cmorrell <cmorrell@opentable.com>
2016-01-16 16:42:11 -0800

    Split up claimPickupRequest into smaller parts

--- cloud/main.js
+++ cloud/main.js
@@ -2,50 +2,6 @@ require("cloud/app.js");
 
 require("cloud/login.js");
 
-Parse.Cloud.define("claimPickupRequest", function (req, res) {
-    if (req.params.pickupRequestId) {
-        var volunteer = req.user;
-        var donor = new Parse.User();
-        var PickupRequest = Parse.Object.extend("PickupRequest");
-        var query = new Parse.Query(PickupRequest);
-        query.equalTo('objectId', req.params.pickupRequestId + "");
-        query.first().then(function (pickupRequest) {
-            // Update the pickupRequest object with the pending volunteer.
-            pickupRequest.set("pendingVolunteer", volunteer);
-
-            donor.id = pickupRequest.get("donor").id;
-
-            // Let the donor read the volunteer's user object, so that she can read the volunteer's name.
-            var volACL = new Parse.ACL(volunteer);
-            volACL.setReadAccess(donor.id, true);
-            volunteer.setACL(volACL);
-            Parse.Object.saveAll([pickupRequest, volunteer]).then(function () {
-                var volunteerName = volunteer.get("name");
-
-                // Send a push to the donor notifying them their pickup request has been claimed.
-                generatePushToUser(donor,
-                    {
-                        "loc-key": "notif_pickup_request_claimed_title",
-                        "loc-args": []
-                    },
-                    {
-                        "loc-key": "notif_pickup_request_claimed_msg",
-                        "loc-args": volunteerName ? [volunteerName] : [],
-                        "action-loc-key": "rsp"
-                    },
-                    "claimPickupRequest").then(function () {
-                    res.success("Request claimed. ACL of volunteer " + volunteer.id + " updated to allow read access for user " + donor.id + ". claimPickupRequest Push Notification sent to " + donor.id);
-                }, function (err) {
-                    res.error(err);
-                });
-            });
-        });
-    } else {
-        res.error('Invalid parameters.');
-    }
-});
-
-
 Parse.Cloud.define("confirmVolunteer", function (req, res) {
     if (req.params.pickupRequestId) {
         var donor = req.user;
