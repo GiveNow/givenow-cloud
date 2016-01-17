878eee04a2cdc7b8e92f93953569f07adc64acd0
Afik Cohen <aphex@aphex.cx>
2016-01-16 17:01:10 -0800

    Review and lint fixes, need to check out of scope variable on L21

--- cloud/services/PickupService.js
+++ cloud/services/PickupService.js
@@ -1,92 +1,90 @@
-
 Parse.Cloud.define("claimPickupRequest", function (request, response) {
-  claimPickup().then(function(result) {
-    response.success(result);
-  }, function(error) {
-    response.error(error);
-  });
+    claimPickup().then(function (result) {
+        response.success(result);
+    }, function (error) {
+        response.error(error);
+    });
+});
 
-let handleError = function(promise) {
-  var fn = function(error) {
-    console.log("error: " + error);
-    promise.reject();
-  }
-
-  return fn;
+let handleError = function (promise) {
+    return function (error) {
+        console.log("error: " + error);
+        promise.reject();
+    };
 };
 
-let claimPickup = function() {
-  var promise = new Parse.Promise();
-  var handleError = handleError(promise);
+let claimPickup = function () {
+    var promise = new Parse.Promise();
+    var handleError = handleError(promise);
 
-  if (!req.params.pickupRequestId) {
-    return promise.reject('Invalid parameters.');
-  }
+    //TODO req is not in scope here
+    if (!req.params.pickupRequestId) {
+        return promise.reject('Invalid parameters.');
+    }
 
-  var volunteer = req.user;
-  var donor = new Parse.User();
-  var pickupRequest;
+    var volunteer = req.user;
+    var donor = new Parse.User();
+    var pickupRequest;
 
-  return getPickupRequest().then( function(pickup) {
-    pickupRequest = pickup
-    return setPickupVolunteer(pickupRequest, volunteer)
-  }).then( function() {
-    return setDonorPermissions(pickupRequest, volunteer)
-  }).then( function() {
-    return sendPushToDonor()
-  }).then(function () {
-      promise.resolve("Request claimed. ACL of volunteer " + volunteer.id + " updated to allow read access for user " + donor.id + ". claimPickupRequest Push Notification sent to " + donor.id);
-  }, function (err) {
-      promise.reject(err);
-  });
+    getPickupRequest().then(function (pickup) {
+        pickupRequest = pickup;
+        return setPickupVolunteer(pickupRequest, volunteer)
+    }).then(function () {
+        return setDonorPermissions(pickupRequest, volunteer)
+    }).then(function () {
+        return sendPushToDonor()
+    }).then(function () {
+        promise.resolve("Request claimed. ACL of volunteer " + volunteer.id + " updated to allow read access for user " + donor.id + ". claimPickupRequest Push Notification sent to " + donor.id);
+    }, function (err) {
+        promise.reject(err);
+    });
 
-  return promise;
+    return promise;
 };
 
-let getPickupRequest = function() {
-  var PickupRequest = Parse.Object.extend("PickupRequest");
-  var query = new Parse.Query(PickupRequest);
-  query.equalTo('objectId', req.params.pickupRequestId + "");
+let getPickupRequest = function () {
+    var PickupRequest = Parse.Object.extend("PickupRequest");
+    var query = new Parse.Query(PickupRequest);
+    query.equalTo('objectId', req.params.pickupRequestId + "");
 
-  return query.first()
+    return query.first()
 };
 
-let setPickupVolunteer = function(pickupRequest, volunteer) {
-  pickupRequest.set("pendingVolunteer", volunteer);
+let setPickupVolunteer = function (pickupRequest, volunteer) {
+    pickupRequest.set("pendingVolunteer", volunteer);
 
-  var promise = new Parse.Promise().as(donor.id);
-  return promise;
+    return new Parse.Promise().as(donor.id);
 };
 
-let setDonorPermissions = function(pickupRequest, volunteer) {
-  var promise = new Parse.Promise();
-  // Let the donor read the volunteer's user object, so that she can read the volunteer's name.
-  var volACL = new Parse.ACL(volunteer);
-  var donor.id = pickupRequest.get("donor").id;
+let setDonorPermissions = function (pickupRequest, volunteer) {
+    var promise = new Parse.Promise();
+    // Let the donor read the volunteer's user object, so that she can read the volunteer's name.
+    var volACL = new Parse.ACL(volunteer);
+    var donor = {id: pickupRequest.get("donor").id};
 
-  volACL.setReadAccess(donor.id, true);
-  volunteer.setACL(volACL);
-  return promise.resolve();
+    volACL.setReadAccess(donor.id, true);
+    volunteer.setACL(volACL);
+    return promise.resolve();
 };
 
-let savePickupAndVolunteer = function(pickupRequest, volunteer){
-  Parse.Object.saveAll([pickupRequest, volunteer]);
+let savePickupAndVolunteer = function (pickupRequest, volunteer) {
+    Parse.Object.saveAll([pickupRequest, volunteer]);
 };
 
-let notifyDonorPickupClaimed = function(pickupRequest, volunteer) {
-  var donor.id = pickupRequest.get("donor").id;
-  var volunteerName = volunteer.get("name");
-  // Send a push to the donor notifying them their pickup request has been claimed.
-  generatePushToUser(donor,
-    {
-      "loc-key": "notif_pickup_request_claimed_title",
-      "loc-args": []
-    },
-    {
-      "loc-key": "notif_pickup_request_claimed_msg",
-      "loc-args": volunteerName ? [volunteerName] : [],
-      "action-loc-key": "rsp"
-    },
-    "claimPickupRequest");
+let notifyDonorPickupClaimed = function (pickupRequest, volunteer) {
+    var donor = {id: pickupRequest.get("donor").id};
+    var volunteerName = volunteer.get("name");
+    // Send a push to the donor notifying them their pickup request has been claimed.
+    generatePushToUser(donor,
+        {
+            "loc-key": "notif_pickup_request_claimed_title",
+            "loc-args": []
+        },
+        {
+            "loc-key": "notif_pickup_request_claimed_msg",
+            "loc-args": volunteerName ? [volunteerName] : [],
+            "action-loc-key": "rsp"
+        },
+        "claimPickupRequest");
 };
 
