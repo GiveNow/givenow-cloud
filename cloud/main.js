a92219ca6fc67a3f038efb9bfa1f9b9d3cc45ffe
Afik Cohen <aphex@aphex.cx>
2016-01-19 01:18:24 -0800

    Migrate marking pickup request complete to cloud code. givenow/givenow-cloud#1 Implement ACL convenience functions.

--- cloud/main.js
+++ cloud/main.js
@@ -98,6 +98,18 @@ Parse.Cloud.define("confirmVolunteer", function (req, res) {
     }
 });
 
+var addIdToACL = function (id, acl) {
+    console.log("add " + id + " to acl " + acl);
+    acl.setReadAccess(id, true);
+    return acl;
+};
+
+var removeIdFromACL = function (id, acl) {
+    console.log("remove " + id + " from acl " + acl);
+    acl.setReadAccess(id, false);
+    return acl;
+};
+
 Parse.Cloud.define("pickupDonation", function (req, res) {
     if (req.params.pickupRequestId) {
         var volunteer = req.user;
@@ -106,53 +118,86 @@ Parse.Cloud.define("pickupDonation", function (req, res) {
         var query = new Parse.Query(PickupRequest);
         query.equalTo('objectId', req.params.pickupRequestId + "");
         query.first().then(function (pickupRequest) {
-            donor.id = pickupRequest.get("donor").id;
+            donor = pickupRequest.get("donor");
 
-            // Create new Donation object
+            // Create new Donation object, readable by the volunteer and donor
             var Donation = Parse.Object.extend("Donation");
             var newDonation = new Donation();
             newDonation.set("donor", donor);
             newDonation.set("donationCategories", pickupRequest.get("donationCategories"));
-            // Readable by the volunteer and donor
-            var donationACL = new Parse.ACL();
-            donationACL.setReadAccess(volunteer.id, true);
-            donationACL.setReadAccess(donor.id, true);
-            newDonation.setACL(donationACL);
+            newDonation.setACL(addIdToACL(volunteer.id, addIdToACL(donor.id, new Parse.ACL())));
             console.log("newDonationSet");
 
-            newDonation.save().then(function (donation) {
-                console.log("saving pickup request");
+            // Remove donor id from volunteer ACL
+            volunteer.set("ACL", removeIdFromACL(donor.id, volunteer.get("ACL")));
 
-                pickupRequest.set("donation", donation);
-                pickupRequest.save().then(function () {
-                    var volunteerName = volunteer.get("name");
-                    console.log("newPickupREquestSaved");
+            Parse.Object.saveAll([donor, volunteer]).then(function () {
+                newDonation.save().then(function (donation) {
+                    console.log("saving pickup request");
 
-                    // Send a push to the donor notifying them their pickup request has been picked up
-                    generatePushToUser(donor,
-                        {
-                            "loc-key": "notif_pickup_complete_title",
-                            "loc-args": []
-                        },
-                        {
-                            "loc-key": "notif_pickup_complete_msg",
-                            "loc-args": volunteerName ? [volunteerName] : [],
-                            "action-loc-key": "rsp"
-                        },
-                        "pickupDonation").then(function () {
-                        res.success("Request picked up. New donation object " + donation.id + " created. pickupDonation Push Notification sent to " + donor.id);
-                    }, function (err) {
-                        res.error(err);
+                    pickupRequest.set("donation", donation);
+                    pickupRequest.save().then(function () {
+                        var volunteerName = volunteer.get("name");
+                        console.log("newPickupRequestSaved");
+
+                        // Send a push to the donor notifying them their pickup request has been picked up
+                        generatePushToUser(donor,
+                            {
+                                "loc-key": "notif_pickup_complete_title",
+                                "loc-args": []
+                            },
+                            {
+                                "loc-key": "notif_pickup_complete_msg",
+                                "loc-args": volunteerName ? [volunteerName] : [],
+                                "action-loc-key": "rsp"
+                            },
+                            "pickupDonation").then(function () {
+                            res.success("Request picked up. New donation object " + donation.id + " created. pickupDonation Push Notification sent to " + donor.id);
+                        }, function (err) {
+                            res.error(err);
+                        });
                     });
+                }, function (err) {
+                    res.error(err)
                 });
-            }, function (err) {
-                res.error(err)
             });
         });
     } else {
         res.error('Invalid parameters.');
     }
 });
+
+Parse.Cloud.define("markComplete", function (req, res) {
+    if (req.params.pickupRequestId) {
+        var donor = req.user;
+        var PickupRequest = Parse.Object.extend("PickupRequest");
+        var query = new Parse.Query(PickupRequest);
+        query.equalTo('objectId', req.params.pickupRequestId + "");
+        query.equalTo('donor', donor);
+        query.first().then(function (pickupRequest) {
+
+            // Mark the pickupRequest object as complete. Currently, by setting it to inactive.
+            pickupRequest.set("isActive", false);
+            pickupRequest.save().then(function () {
+                var volunteer = pickupRequest.get("confirmedVolunteer");
+
+                // Remove volunteer id from donor ACL
+                console.log("donor id " + donor.id);
+                donor.set("ACL", removeIdFromACL(volunteer.id, donor.get("ACL")));
+
+                donor.save().then(function () {
+                    res.success("Pickup request " + pickupRequest.id + " marked complete.");
+                }, function (err) {
+                    res.error(err);
+                });
+            });
+        });
+    }
+    else {
+        res.error('Invalid parameters.');
+    }
+    }
+);
 
 
 function generatePushToUser(user, title, alert, type) {
