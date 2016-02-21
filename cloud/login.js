b3c39ab752d1ac8bed975490342e182d02beb51f
Afik Cohen <aphex@aphex.cx>
2016-02-20 23:39:07 -0800

    Add magic phone number for demo purposes

--- cloud/login.js
+++ cloud/login.js
@@ -2,6 +2,10 @@
 //var languages = ["en", "es", "ja", "kr", "pt-BR"];
 var smsService = require("cloud/services/SmsService.js");
 
+//Magic phone number and login. For use for demo purposes.
+// +91 91919 19191
+const MAGIC_PHONE_NUMBER = "919191919191";
+const MAGIC_CODE = "9191";
 
 Parse.Cloud.define("sendCode", function (req, res) {
     var phoneNumber = req.params.phoneNumber;
@@ -16,30 +20,29 @@ Parse.Cloud.define("sendCode", function (req, res) {
     Parse.Cloud.useMasterKey();
     var query = new Parse.Query(Parse.User);
     query.equalTo('username', phoneNumber + "");
-    query.first().then(function (result) {
+    query.first().then(function (existingUser) {
         var min = 1000;
         var max = 9999;
-        var num = Math.floor(Math.random() * (max - min + 1)) + min;
+        var code = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        if (phoneNumber == MAGIC_PHONE_NUMBER) {
+            code = MAGIC_CODE;
+        }
         Parse.Config.get().then(function (config) {
-            if (result) {
-                result.setPassword(config.get("secretPasswordToken") + num);
-                //			result.set("language", language);
-                result.save().then(function () {
-                    return smsService.sendCodeSms(phoneNumber, num, body);
+            var password = config.get("secretPasswordToken") + code;
+            if (existingUser) {
+                existingUser.setPassword(password);
+                existingUser.save().then(function () {
+                    return smsService.sendCodeSms(phoneNumber, code, body);
                 }).then(function () {
                     res.success("User " + phoneNumber + " exists.");
                 }, function (err) {
                     res.error(err);
                 });
             } else {
-                var user = new Parse.User();
-                user.setUsername(phoneNumber);
-                user.setPassword(config.get("secretPasswordToken") + num);
-                //			user.set("language", language);
-                user.setACL({});
-                user.save().then(function (savedUser) {
-                    return smsService.sendCodeSms(phoneNumber, num, body);
-                }).then(function () {
+                createUser(phoneNumber, password).then(function (newUser) {
+                        return smsService.sendCodeSms(phoneNumber, code, body);
+                    }).then(function () {
                     res.success("Created new user " + phoneNumber);
                 }, function (err) {
                     res.error(err);
@@ -50,6 +53,14 @@ Parse.Cloud.define("sendCode", function (req, res) {
         res.error(err);
     });
 });
+
+var createUser = function (phoneNumber, password) {
+    var user = new Parse.User();
+    user.setUsername(phoneNumber);
+    user.setPassword(password);
+    user.setACL({});
+    return user.save();
+};
 
 Parse.Cloud.define("logIn", function (req, res) {
     Parse.Cloud.useMasterKey();
